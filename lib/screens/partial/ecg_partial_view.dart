fd76cf6f9d3a9655ea4ed0db1db55ca6459e26d3
Magno Bustamante <magpex13@github.com>
2021-11-25 14:58:11 -0500

    Se muestra gráfico estadístico según datos que envía el wearable en tiempo real durante 1 minuto; Se juntan todos los datos de ECG para enviarse al servicio de G Cloud

--- lib/screens/partial/ecg_partial_view.dart
+++ lib/screens/partial/ecg_partial_view.dart
@@ -1,10 +1,10 @@
-import 'dart:async';
-import 'dart:io';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:gcloud/pubsub.dart';
 import 'package:googleapis_auth/auth_io.dart' as auth;
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:polar/polar.dart';
+import 'package:intl/intl.dart';
 
 class EcgPartialView extends StatefulWidget {
   EcgPartialView({Key? key}) : super(key: key);
@@ -14,6 +14,7 @@ class EcgPartialView extends StatefulWidget {
 }
 
 class _EcgPartialView extends State<EcgPartialView> {
+  final identifier = '7E1B542A';
   final Color colorLine = Colors.redAccent;
 
   final limitCount = 100;
@@ -21,84 +22,93 @@ class _EcgPartialView extends State<EcgPartialView> {
 
   double xValue = 0;
   double step = 0.04;
+  bool _firstTime = true;
+  String _textState =
+      "Active el Bluetooth y colóquese el dispositivo en el pecho para empezar por favor";
+  List<int> joinedECGdata = <int>[];
 
-  late Timer timer;
+  //late Timer timer;
+  late final Polar polar;
 
-  List<int> testData = <int>[
-    -198,
-    -206,
-    -203,
-    -206,
-    -215,
-    -206,
-    -201,
-    -218,
-    -218,
-    -208,
-    -225,
-    -240,
-    -227,
-    -220,
-    -215,
-    -194,
-    -184,
-    -194,
-    -196,
-    -196,
-    -191,
-    -174,
-    -169,
-    -184,
-    -196,
-    -186,
-    -152,
-    -116,
-    -101,
-    -128,
-    -155,
-    -157,
-    -150,
-    -150,
-    -150,
-    -157,
-    -189,
-    -206,
-    -186,
-    -143,
-    -9,
-    264,
-    417,
-    106,
-    -375,
-    -446,
-    -257,
-    -225,
-    -254,
-    -227,
-    -225,
-    -240,
-    -237,
-    -249,
-    -259,
-    -252,
-    -252,
-    -254,
-    -242,
-    -225,
-    -215,
-    -210,
-    -208,
-    -206,
-    -196,
-    -184,
-    -181,
-    -198,
-    -203,
-    -184,
-    -174,
-    -172,
-    -164
-  ];
+  // List<int> testData = <int>[
+  //   -198,
+  //   -206,
+  //   -203,
+  //   -206,
+  //   -215,
+  //   -206,
+  //   -201,
+  //   -218,
+  //   -218,
+  //   -208,
+  //   -225,
+  //   -240,
+  //   -227,
+  //   -220,
+  //   -215,
+  //   -194,
+  //   -184,
+  //   -194,
+  //   -196,
+  //   -196,
+  //   -191,
+  //   -174,
+  //   -169,
+  //   -184,
+  //   -196,
+  //   -186,
+  //   -152,
+  //   -116,
+  //   -101,
+  //   -128,
+  //   -155,
+  //   -157,
+  //   -150,
+  //   -150,
+  //   -150,
+  //   -157,
+  //   -189,
+  //   -206,
+  //   -186,
+  //   -143,
+  //   -9,
+  //   264,
+  //   417,
+  //   106,
+  //   -375,
+  //   -446,
+  //   -257,
+  //   -225,
+  //   -254,
+  //   -227,
+  //   -225,
+  //   -240,
+  //   -237,
+  //   -249,
+  //   -259,
+  //   -252,
+  //   -252,
+  //   -254,
+  //   -242,
+  //   -225,
+  //   -215,
+  //   -210,
+  //   -208,
+  //   -206,
+  //   -196,
+  //   -184,
+  //   -181,
+  //   -198,
+  //   -203,
+  //   -184,
+  //   -174,
+  //   -172,
+  //   -164
+  // ];
+
+  List<int> prepareECGdata() {
+    return <int>[];
+  }
 
   void sentToGCloud() async {
     // Read the service account credentials from the file.
@@ -116,18 +126,22 @@ class _EcgPartialView extends State<EcgPartialView> {
     var pubsub = new PubSub(client, 'systemheart');
     var topic =
         await pubsub.lookupTopic('projects/systemheart/topics/lectureECG');
-    var dummyData = {
-      'data': [40, 50, 50, 80],
-      'read_rate': '2021-11-04',
-      'user': 1
-    };
-    print(dummyData['data'].toString());
+    // var dummyData = {
+    //   'data': [40, 50, 50, 80],
+    //   'read_date': '2021-11-04',
+    //   'user': 1
+    // };
+    // print(dummyData['data'].toString());
 
-    var response = await topic.publishString(dummyData.toString(), attributes: {
-      "data": dummyData['data'].toString(),
-      'read_rate': dummyData['read_rate'].toString(),
-      'user': dummyData['user'].toString()
-    });
+    var currentDatetime = DateTime.now();
+    var response = await topic.publishString(
+        'Envío de datos del wearable con fecha de ' +
+            DateFormat('dd-MM-yyyy:H:m:s').format(currentDatetime),
+        attributes: {
+          "data": joinedECGdata.toString(),
+          'read_date': DateFormat('yyyy-MM-dd').format(currentDatetime),
+          'user': '1'
+        });
 
     print(response);
   }
@@ -135,39 +149,74 @@ class _EcgPartialView extends State<EcgPartialView> {
   @override
   void initState() {
     super.initState();
-    sentToGCloud();
-    timer = Timer.periodic(const Duration(milliseconds: 1000), (timer) {
-      while (points.length > limitCount) {
-        points.removeAt(0);
+
+    polar = Polar();
+    polar.deviceConnectingStream.listen((_) => setState(() {
+          _textState = "Conectando";
+        }));
+    polar.deviceConnectedStream.listen((_) => setState(() {
+          _textState = "Conectado!";
+        }));
+    var currentTimestamp = 0;
+    polar.streamingFeaturesReadyStream.listen((e) {
+      if (e.features.contains(DeviceStreamingFeature.ecg)) {
+        polar.startEcgStreaming(e.identifier).listen((e) {
+          if (_firstTime) {
+            currentTimestamp = e.timeStamp;
+            _firstTime = false;
+          }
+
+          while (points.length > limitCount) {
+            points.removeAt(0);
+          }
+
+          joinedECGdata.addAll(e.samples);
+          for (var i = 0; i < e.samples.length; i++) {
+            points.add(FlSpot(xValue, e.samples[i] / 1000.0));
+            xValue += step;
+          }
+
+          if ((e.timeStamp - currentTimestamp) / 1000000000 >= 60) {
+            // 1 minuto
+            polar.disconnectFromDevice(identifier);
+          }
+
+          setState(() {
+            print('ECG data: ${e.samples}');
+          });
+        });
       }
-      for (var i = 0; i < testData.length; i++) {
-        points.add(FlSpot(xValue, testData[i] / 1000.0));
-        xValue += step;
-      }
-      setState(() {});
     });
+    polar.deviceDisconnectedStream.listen((_) {
+      sentToGCloud();
+      setState(() {
+        _textState = "Desconectado";
+      });
+    });
+    polar.connectToDevice(identifier);
+    //sentToGCloud();
+    //timer = Timer.periodic(const Duration(milliseconds: 1000), (timer) {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: points.isNotEmpty
-          ? Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'x: ${xValue.toStringAsFixed(1)}',
-                  style: const TextStyle(
-                    color: Colors.black,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(
-                  height: 12,
-                ),
-                SizedBox(
-                  width: 300,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            _textState,
+            style: const TextStyle(
+              color: Colors.black,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(
+            height: 12,
+          ),
+          points.isNotEmpty
+              ? SizedBox(
                   height: 300,
                   child: LineChart(
                     LineChartData(
@@ -193,9 +242,9 @@ class _EcgPartialView extends State<EcgPartialView> {
                     ),
                   ),
                 )
-              ],
-            )
-          : Container(),
+              : Container()
+        ],
+      ),
     );
   }
 
@@ -214,7 +263,7 @@ class _EcgPartialView extends State<EcgPartialView> {
 
   @override
   void dispose() {
-    timer.cancel();
+    //timer.cancel();
     super.dispose();
   }
 }
